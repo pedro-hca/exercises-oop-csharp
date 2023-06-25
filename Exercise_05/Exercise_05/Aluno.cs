020fd0ed7c693e8cfef94d5d12276430882233dd
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-25 17:53:56 -0300

    Exercise_05 solution

---
+++ Exercise_05/Exercise_05/Aluno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_05
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double Media()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public double NotaRestante()
+        {
+            return 60.0 - Media();
+        }
+
+        public void NotaFinal()
+        {
+            if (Media() > 60)
+            {
+                Console.WriteLine("NOTA FINAL = " + Media().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("NOTA FINAL = " + Media().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+    }
+}
---
+++ Exercise_05/Exercise_05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            aluno.NotaFinal();
+        }
+    }
+}
