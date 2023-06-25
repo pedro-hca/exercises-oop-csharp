b0c182ee302e1ec84cdf90fa39f1e09878e50ef1
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-25 17:20:13 -0300

    Exercise_04 solution

---
+++ Exercise_04/Exercise_04/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_04
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentaSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * (porcentagem / 100));
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercise_04/Exercise_04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.WriteLine("Digite os dados do funcionario:");
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: " + funcionario);
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentaSalario(porcentagem);
+
+            Console.WriteLine("Dados atualizados: " + funcionario);
+        }
+    }
+}
