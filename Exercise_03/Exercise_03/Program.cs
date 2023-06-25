e8bdef6b4dc9a1ec24fe159b287a8c084b5caac7
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-25 16:47:26 -0300

    Exercise_03 solution

---
+++ Exercise_03/Exercise_03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Digite a largura e altura do retângulo");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(retangulo);
+
+        }
+    }
+}
