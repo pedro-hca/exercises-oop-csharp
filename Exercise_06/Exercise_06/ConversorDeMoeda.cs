d7ef7f6b176814a51892195e5658a6beb26cf2a3
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-25 21:54:05 -0300

    Exercise_06 solution

---
+++ Exercise_06/Exercise_06/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_06
+{
+    internal class ConversorDeMoeda
+    {
+        public static double Iof = 0.06;
+
+        public static double ConverteDolar(double cotacao, double quantidade)
+        {
+            return (cotacao * quantidade) + ((cotacao * quantidade) * Iof);
+        }
+
+
+
+    }
+}
---
+++ Exercise_06/Exercise_06/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = ConversorDeMoeda.ConverteDolar(cotacao, quantidade);
+
+            Console.WriteLine("Valor a ser pago em reais = " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
