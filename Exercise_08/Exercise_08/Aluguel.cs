1c28b3d05006ad51fcdb13bc3bd6d0e9eaa793a4
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-28 23:10:38 -0300

    Exercise_08 solution

---
+++ Exercise_08/Exercise_08/Aluguel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace Exercise_08
+{
+    internal class Aluguel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Aluguel(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString()
+        {
+            return Quarto + ": " + Nome + ", " + Email;
+        }
+    }
+}
---
+++ Exercise_08/Exercise_08/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluguel[] vetor = new Aluguel[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Aluguel #{i + 1}: ");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vetor[quarto] = new Aluguel(nome, email, quarto);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vetor[i] != null)
+                {
+                    Console.WriteLine(vetor[i].ToString());
+                }
+            }
+        }
+    }
+}
+
