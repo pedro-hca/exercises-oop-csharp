b9cdd940bdfccef4367c2f023634353c53c4a2c3
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-29 22:46:50 -0300

    Exercise_09 solution

---
+++ Exercise_09/Exercise_09/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exercise_09
+{
+    internal class Employee
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary = Salary + (Salary * percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
